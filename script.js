6ef86255e4cb9f0a5ea58bdb0367da40f7da52cf
kowhlwa <hyunssl101@gmail.com>
2026-01-17 23:01:21 -0500

    Added functionality to input text box, cancel button, and confirm button

--- script.js
+++ script.js
@@ -1,10 +1,5 @@
-// Code for generating 16x16 grid canvas
-const canvas = document.querySelector(".container");
-for (let idx = 0; idx < 256; idx++) {
-    const block = document.createElement("div");
-    block.classList.add("zone");
-    canvas.appendChild(block);
-}
+// Initial code for generating a 16 x 16 canvas
+generateCanvas(16);
 
 // Event Listener for mouse hovering over the grid boxes
 const boxList = document.querySelectorAll(".zone");
@@ -19,3 +14,35 @@ modal.classList.toggle("hidden");
 // Event Listener for modal window button
 const modalButton = document.querySelector("#canvas-option");
 modalButton.addEventListener("click", () => modal.classList.toggle("hidden"));
+
+// Handle cancel
+const cancelModal = document.querySelector(".option-modal-cancel");
+cancelModal.addEventListener("click", () => modal.classList.toggle("hidden"));
+
+// Handle confirm dimension changes
+const confirmModal = document.querySelector(".option-modal-confirm");
+confirmModal.addEventListener("click", () => {
+    const checkInput = document.querySelector(".option-modal-response");
+    generateCanvas(checkInput.value);
+    modal.classList.toggle("hidden");
+});
+
+// Function generates new canvas based on (N = squares), (NxN) grid of squares.
+function generateCanvas(squares) {
+    const canvas = document.querySelector(".container");
+
+    // First empty out the old canvas
+    if (canvas.firstChild) {
+        canvas.replaceChildren();
+    }
+
+    // Generate N x N grid of blocks within the canvas
+    if (squares <= 100) {
+        for (let idx = 0; idx < squares**2; idx++) {
+            const block = document.createElement("div");
+            block.classList.add("zone");
+            block.addEventListener("mouseenter", () => block.classList.add("zone-colored"));
+            canvas.appendChild(block);
+        }
+    }
+}
